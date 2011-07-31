d908ccddc625da2ff45caa48ecdf934b46e0baf2
Michael Guterl <michael@diminishing.org>
2011-07-31 16:31:36 -0400

    fix whitespace

--- spec/pinboard/client_spec.rb
+++ spec/pinboard/client_spec.rb
@@ -8,19 +8,19 @@ describe Pinboard::Client do
     before do
       stub_get("posts/all").
         to_return(:body => fixture("posts_all.xml"),
-                  :headers => { 'content-type' => 'text/xml' })
+        :headers => { 'content-type' => 'text/xml' })
     end
 
     it "returns a collection of posts" do
-       client.all_posts.count.should == 2
+      client.all_posts.count.should == 2
     end
 
     it "loads posts with valid attributes" do
-       post = client.all_posts.first
-       post.href.should_not be_nil
-       post.description.should_not be_nil
-       post.tag.should_not be_nil
-       post.time.should_not be_nil
+      post = client.all_posts.first
+      post.href.should_not be_nil
+      post.description.should_not be_nil
+      post.tag.should_not be_nil
+      post.time.should_not be_nil
     end
 
   end
