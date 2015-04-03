e96c7d29e5eefe4ac178ef911af2ff83cbbb8c26
Dima Kalachov <dima.kalachov@gmail.com>
2015-04-03 23:57:20 +0300

    Specs for new methods.

--- spec/pinboard/client_spec.rb
+++ spec/pinboard/client_spec.rb
@@ -339,4 +339,140 @@ describe Pinboard::Client do
       notes.first.title.should == "Paul Graham on Hirin' The Ladies"
     end
   end
+
+  describe "#user_secret" do
+    let(:client) { Pinboard::Client.new(auth_params) }
+    before do
+      stub_get("user/secret?").
+        to_return(:body => fixture("user_secret.xml"),
+                  :headers => { 'content-type' => 'text/xml' })
+    end
+
+    it "returns the user's secret RSS key" do
+      secret = client.user_secret
+      secret.should == "6493a84f72d86e7de130"
+    end
+  end
+
+  describe "#user_api_token" do
+    let(:client) { Pinboard::Client.new(auth_params) }
+    before do
+      stub_get("user/api_token?").
+          to_return(:body => fixture("user_api_token.xml"),
+                    :headers => { 'content-type' => 'text/xml' })
+    end
+
+    it "returns the user's API token" do
+      token = client.user_api_token
+      token.should == "XOG86E7JIYMI"
+    end
+  end
+
+  describe "#get" do
+    let(:client) { Pinboard::Client.new(auth_params) }
+
+    context "when there are many posts" do
+      before do
+        stub_get("posts/get?").
+            to_return(:body => fixture("multiple_posts.xml"),
+                      :headers => { 'content-type' => 'text/xml' })
+      end
+
+      it "returns a collection of posts" do
+        expected =
+            [
+                Pinboard::Post.new(
+                    :href => "http://foo.com/",
+                    :description => "Foo!",
+                    :extended => "long description Foo",
+                    :tag => 'foo bar',
+                    :toread => 'yes',
+                    :time => Time.parse("2011-07-26T17:52:04Z")),
+                Pinboard::Post.new(
+                    :href => "http://bar.com/",
+                    :description => "Bar!",
+                    :extended => "long description Bar",
+                    :tag => 'foo bar',
+                    :toread => 'yes',
+                    :time => Time.parse("2011-07-26T17:52:04Z")),
+            ]
+
+        client.get.should == expected
+      end
+    end
+
+    context "where there is a single post" do
+      before do
+        stub_get("posts/get?").
+            to_return(:body => fixture("single_post.xml"),
+                      :headers => { 'content-type' => 'text/xml' })
+      end
+
+      it "still returns an array" do
+        client.get.class.should == Array
+      end
+    end
+
+    context "when there are no posts" do
+      before do
+        stub_get("posts/get?").
+            to_return(:body => fixture("no_posts.xml"),
+                      :headers => { 'content-type' => 'text/xml' })
+      end
+
+      it "returns an empty array" do
+        client.get.should == []
+      end
+    end
+  end
+
+  describe "#suggest" do
+    let(:client) { Pinboard::Client.new(auth_params) }
+    let(:url) { 'http://example.com' }
+
+    context "when there are many popular and recommended tags" do
+      before do
+        stub_get("posts/suggest").
+            with(:query => { 'url' => url }).
+            to_return(:body => fixture("multiple_suggest.xml"),
+                      :headers => { 'content-type' => 'text/xml' })
+      end
+
+      it "returns a collections of tags" do
+        suggest = client.suggest(url)
+        suggest[:popular].should == %w[blog blogs]
+        suggest[:recommended].should == %w[writing weblog]
+      end
+    end
+
+    context "when there are single popular and recommended tags" do
+      before do
+        stub_get("posts/suggest?").
+            with(:query => { 'url' => url }).
+            to_return(:body => fixture("single_suggest.xml"),
+                      :headers => { 'content-type' => 'text/xml' })
+      end
+
+      it "still returns an array" do
+        suggest = client.suggest(url)
+        suggest[:popular].class.should == Array
+        suggest[:recommended].class.should == Array
+      end
+    end
+
+    context "when there are no popular and recommended tags" do
+      before do
+        stub_get("posts/suggest?").
+            with(:query => { 'url' => url }).
+            to_return(:body => fixture("no_suggest.xml"),
+                      :headers => { 'content-type' => 'text/xml' })
+      end
+
+      it "returns an empty array" do
+        suggest = client.suggest(url)
+        suggest[:popular].should == []
+        suggest[:recommended].should == []
+      end
+    end
+  end
 end
